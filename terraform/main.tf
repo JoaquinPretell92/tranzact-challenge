b0c80279072f7f8eb27a2a6594b8dd68a0bb0bba
unknown <joaquin.pretell@tecsup.edu.pe>
2023-01-11 14:12:23 -0500

    prueba

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,7 +45,7 @@ resource "azurerm_app_service_source_control" "example" {
 }
 
 resource "azurerm_log_analytics_workspace" "example" {
-  name                = "tranzact-challenge-workspace"
+  name                = "tranzact-challenge-workspaces"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   sku                 = "PerGB2018"
